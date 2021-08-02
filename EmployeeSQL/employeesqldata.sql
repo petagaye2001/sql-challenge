c57761b918189d5bb06ab30f9c0f08df5803d0c6
petagayesimone <petagaye2001@yahoo.com>
2021-08-01 22:10:36 -0400

    updated files

--- EmployeeSQL/employeesqldata.sql
+++ EmployeeSQL/employeesqldata.sql
@@ -8,42 +8,47 @@ CREATE TABLE departments(
 --Create table with department employee info
 CREATE TABLE dept_emp(
   id SERIAL,
-  emp_no VARCHAR(10) NOT NULL,
+  	emp_no VARCHAR(10) NOT NULL,
 	 dept_no VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 
---Create table with department employee info
+--Create table with department manager info
 CREATE TABLE dept_manager(
   id SERIAL,
-  emp_no VARCHAR(10) NOT NULL,
-	 dept_no VARCHAR(10) NOT NULL,
+  dept_no VARCHAR(10) NOT NULL,
+	 emp_no VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 
---Create table with department employee info
+--Create table with employee info
 CREATE TABLE employees(
   id SERIAL,
-  emp_no VARCHAR(10) NOT NULL,
-	 dept_no VARCHAR(10) NOT NULL,
+  	emp_no VARCHAR(10) NOT NULL,
+	 emp_title_id VARCHAR(10) NOT NULL,
+	birth_date VARCHAR(10) NOT NULL,
+	first_name VARCHAR(10) NOT NULL,
+	last_name VARCHAR(10) NOT NULL,
+	sex VARCHAR(10) NOT NULL,
+	hire_date VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 
---Create table with department employee info
+--Create table with salaries employee info
 CREATE TABLE salaries (
   id SERIAL,
   emp_no VARCHAR(10) NOT NULL,
-	 dept_no VARCHAR(10) NOT NULL,
+	 salary VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 	
---Create table with department employee info
+--Create table with title info
 CREATE TABLE titles (
   id SERIAL,
-  emp_no VARCHAR(10) NOT NULL,
-	 dept_no VARCHAR(10) NOT NULL,
+  title_id VARCHAR(10) NOT NULL,
+	 title VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 		
